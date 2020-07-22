605ae5a4d78ab594302648b3cb8893c80dafff72
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2020-07-22 11:16:26 +0200

    🤸‍♂️ Initial commit.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="hello">
+        Hello.
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String,
+        },
+    };
+</script>
